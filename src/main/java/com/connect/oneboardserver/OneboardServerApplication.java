757237d038b5d6c2c6715580fcac145c59be4a14
shy625 <thgusdud123@gmail.com>
2021-10-22 00:30:28 +0900

    Init Project

---
+++ src/main/java/com/connect/oneboardserver/OneboardServerApplication.java
@@ -0,0 +1,13 @@
+package com.connect.oneboardserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneboardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneboardServerApplication.class, args);
+	}
+
+}
