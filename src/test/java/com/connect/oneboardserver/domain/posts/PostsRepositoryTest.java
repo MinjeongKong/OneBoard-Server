952c64706497f4f528b0b90d08a82fbea9f5a972
shy625 <thgusdud123@gmail.com>
2021-10-28 05:00:32 +0900

    Add test for save post feature
    PostsApiControllerTest 추가
    PostsRepositoryTest import 수정

--- src/test/java/com/connect/oneboardserver/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/connect/oneboardserver/domain/posts/PostsRepositoryTest.java
@@ -1,12 +1,10 @@
 package com.connect.oneboardserver.domain.posts;
 
-import org.aspectj.lang.annotation.After;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
