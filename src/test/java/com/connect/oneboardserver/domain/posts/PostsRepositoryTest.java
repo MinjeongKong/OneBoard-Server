8cd96d37a07b0c69c11800fe455225e4f7690ebf
shy625 <thgusdud123@gmail.com>
2021-10-28 02:58:19 +0900

    Add test for PostsRepository

---
+++ src/test/java/com/connect/oneboardserver/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.connect.oneboardserver.domain.posts;
+
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanUp() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("게시글 저장 및 불러오기")
+    public void savePost() {
+        // given
+        String title = "Test Title";
+        String content = "Test Content";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("shy625")
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
