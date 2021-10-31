7cbc9ae0181254984fd991c8b2be85d2cee0077a
MinjeongKong <kong1301@ajou.ac.kr>
2021-10-31 17:09:19 +0900

    Update MemberRepository

--- src/main/java/com/connect/oneboardserver/domain/login/MemberRepository.java
+++ src/main/java/com/connect/oneboardserver/domain/login/MemberRepository.java
@@ -1,7 +1,8 @@
 package com.connect.oneboardserver.domain.login;
 
+import net.bytebuddy.asm.TypeReferenceAdjustment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
+    Member findByLoginId(String student_num);
 }
