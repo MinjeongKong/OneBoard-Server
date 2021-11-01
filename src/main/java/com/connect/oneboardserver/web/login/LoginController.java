eab39c1e8a3832126fd1dc4499c222f4ecec6d16
MinjeongKong <kong1301@ajou.ac.kr>
2021-11-01 18:06:20 +0900

    Add LoginController

---
+++ src/main/java/com/connect/oneboardserver/web/login/LoginController.java
@@ -0,0 +1,23 @@
+package com.connect.oneboardserver.web.login;
+
+import com.connect.oneboardserver.service.login.LoginService;
+import com.connect.oneboardserver.web.dto.LessonCreateRequestDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class LoginController {
+
+    private final LoginService loginService;
+
+//    @PostMapping("/auth/login")
+//
+//
+//    @GetMapping("/auth/logout")
+
+}
+
