a2e5b49ea1f50dc9711b60a65f58c87892da0979
shy625 <thgusdud123@gmail.com>
2021-11-14 04:25:25 +0900

    Add registerMemberToLecture feature
    개발 시 데이터 입력용

--- src/main/java/com/connect/oneboardserver/service/lecture/LectureService.java
+++ src/main/java/com/connect/oneboardserver/service/lecture/LectureService.java
@@ -3,6 +3,7 @@ package com.connect.oneboardserver.service.lecture;
 import com.connect.oneboardserver.domain.lecture.Lecture;
 import com.connect.oneboardserver.domain.lecture.LectureRepository;
 import com.connect.oneboardserver.domain.login.Member;
+import com.connect.oneboardserver.domain.login.MemberRepository;
 import com.connect.oneboardserver.domain.relation.MemberLecture;
 import com.connect.oneboardserver.domain.relation.MemberLectureRepository;
 import com.connect.oneboardserver.web.dto.ResponseDto;
@@ -21,6 +22,7 @@ import java.util.List;
 public class LectureService {
 
     private final UserDetailsService userDetailsService;
+    private final MemberRepository memberRepository;
     private final LectureRepository lectureRepository;
     private final MemberLectureRepository memberLectureRepository;
 
@@ -37,6 +39,28 @@ public class LectureService {
                 .lectureId(lecture.getId())
                 .build();
         return new ResponseDto("SUCCESS", responseDto);
+    }
+
+    public ResponseDto registerLecture(Long memberId, Long lectureId) {
+        List<MemberLecture> memberLectureList = memberLectureRepository.findAllByMemberId(memberId);
+        for(MemberLecture ml : memberLectureList) {
+            if(ml.getLecture().getId().equals(lectureId)) {
+                throw new IllegalArgumentException("동일한 데이터가 이미 있습니다 : memberId = " + memberId
+                        + " lectureId = " + lectureId);
+            }
+        }
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저가 없습니다 : id = " + memberId));
+
+        Lecture lecture = lectureRepository.findById(lectureId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 과목이 없습니다 : id = " + lectureId));
+
+        MemberLecture memberLecture = memberLectureRepository.save(MemberLecture.builder()
+                        .member(member)
+                        .lecture(lecture)
+                        .build());
+        return new ResponseDto("SUCCESS", memberLecture);
     }
 
     public ResponseDto findLectureList(String email) {
