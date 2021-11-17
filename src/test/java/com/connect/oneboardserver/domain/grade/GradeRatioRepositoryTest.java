6470f81ebc314e03578d3f593d3e99cd00b9c205
MinjeongKong <kong1301@ajou.ac.kr>
2021-11-17 20:08:59 +0900

    Add grade domain test

---
+++ src/test/java/com/connect/oneboardserver/domain/grade/GradeRatioRepositoryTest.java
@@ -0,0 +1,79 @@
+package com.connect.oneboardserver.domain.grade;
+
+import com.connect.oneboardserver.domain.lecture.Lecture;
+import com.connect.oneboardserver.domain.lecture.LectureRepository;
+import com.connect.oneboardserver.domain.relation.GradeRatioLecture;
+import com.connect.oneboardserver.domain.relation.GradeRatioLectureRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class GradeRatioRepositoryTest {
+
+    @Autowired
+    private GradeRatioRepository gradeRatioRepository;
+
+    @Autowired
+    private GradeRatioLectureRepository gradeRatioLectureRepository;
+
+    @Autowired
+    private LectureRepository lectureRepository;
+
+
+    @AfterEach
+    public void cleanUp() {
+        gradeRatioLectureRepository.deleteAll();
+        gradeRatioRepository.deleteAll();
+        lectureRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("학점 비율 입력 및 조회")
+    public void createGradeRatio() {
+        //given
+        String lectureTitle = "test lecture";
+        String lecturePlan = "test url";
+        String semester = "2021-2";
+
+        Lecture lecture = lectureRepository.save(Lecture.builder()
+                .title(lectureTitle)
+                .lecturePlan(lecturePlan)
+                .semester(semester)
+                .build());
+
+        Integer aRatio = 20;
+        Integer bRatio = 50;
+
+        GradeRatio gradeRatio = gradeRatioRepository.save(GradeRatio.builder()
+                .aRatio(aRatio)
+                .bRatio(bRatio)
+                .build());
+
+        gradeRatioLectureRepository.save(GradeRatioLecture.builder()
+                .lecture(lecture)
+                .gradeRatio(gradeRatio)
+                .build());
+
+        // when
+        List<GradeRatio> gradeRatioList = gradeRatioRepository.findAll();
+        List<GradeRatioLecture> gradeRatioLectureList = gradeRatioLectureRepository.findAll();
+
+        // then
+        GradeRatioLecture gradeRatioLecture = gradeRatioLectureList.get(0);
+        GradeRatio gradeRatio1 = gradeRatioList.get(0);
+        GradeRatio gradeRatio2 = gradeRatioLecture.getGradeRatio();
+        assertThat(gradeRatio1.getARatio()).isEqualTo(aRatio);
+        assertThat(gradeRatio1.getBRatio()).isEqualTo(gradeRatio2.getBRatio());
+
+    }
+}
---
+++ src/test/java/com/connect/oneboardserver/domain/grade/GradeRepositoryTest.java
@@ -0,0 +1,91 @@
+package com.connect.oneboardserver.domain.grade;
+
+import com.connect.oneboardserver.domain.lecture.Lecture;
+import com.connect.oneboardserver.domain.lecture.LectureRepository;
+import com.connect.oneboardserver.domain.login.Member;
+import com.connect.oneboardserver.domain.login.MemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class GradeRepositoryTest {
+
+    @Autowired
+    private GradeRepository gradeRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private LectureRepository lectureRepository;
+
+    @AfterEach
+    public void cleanUp() {
+        gradeRepository.deleteAll();
+        memberRepository.deleteAll();
+        lectureRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("성적 부여 및 조회")
+    public void createGrade() {
+        //given
+        String lectureTitle = "test lecture";
+        String lecturePlan = "test url";
+        String semester = "2021-2";
+
+        Lecture lecture = lectureRepository.save(Lecture.builder()
+                .title(lectureTitle)
+                .lecturePlan(lecturePlan)
+                .semester(semester)
+                .build());
+
+        String studentNumber = "201823787";
+        String name = "Minjeong Kong";
+        String password = "0000";
+        String email = "kong1301@ajou.ac.kr";
+        String userType = "S";
+        String university = "Ajou";
+        String major = "Software";
+
+        Member student = memberRepository.save(Member.builder()
+                .studentNumber(studentNumber)
+                .name(name)
+                .password(password)
+                .email(email)
+                .userType(userType)
+                .university(university)
+                .major(major)
+                .roles(Collections.singletonList(userType))
+                .build());
+
+        Float score = 40.3f;
+
+        gradeRepository.save(Grade.builder()
+                .lecture(lecture)
+                .student(student)
+                .score(score)
+                .build());
+
+
+        // when
+        List<Grade> gradeList = gradeRepository.findAll();
+
+        // then
+        Grade grade = gradeList.get(0);
+        assertThat(grade.getStudent().getEmail()).isEqualTo(email);
+        assertThat(grade.getLecture().getId()).isEqualTo(lecture.getId());
+        assertThat(grade.getScore()).isEqualTo(score);
+    }
+}
