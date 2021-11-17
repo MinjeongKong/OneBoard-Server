7502f1b82e916283cd91cb0c72421d06a6a2ab95
MinjeongKong <kong1301@ajou.ac.kr>
2021-11-17 20:10:11 +0900

    Add grade service

---
+++ src/main/java/com/connect/oneboardserver/service/grade/GradeService.java
@@ -0,0 +1,10 @@
+package com.connect.oneboardserver.service.grade;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class GradeService {
+
+}
