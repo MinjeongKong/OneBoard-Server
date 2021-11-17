d79dff2ed8b1078aefb4e8d772b3450b7b49976a
MinjeongKong <kong1301@ajou.ac.kr>
2021-11-18 00:25:55 +0900

    Add GradeRatioApiController

---
+++ src/main/java/com/connect/oneboardserver/web/grade/GradeRatioApiController.java
@@ -0,0 +1,36 @@
+package com.connect.oneboardserver.web.grade;
+
+import com.connect.oneboardserver.service.grade.GradeRatioService;
+import com.connect.oneboardserver.web.dto.ResponseDto;
+import com.connect.oneboardserver.web.dto.grade.GradeRatioCreateRequestDto;
+import com.connect.oneboardserver.web.dto.grade.GradeRatioUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
+@RestController
+public class GradeRatioApiController {
+
+    private final GradeRatioService gradeRatioService;
+
+    //비율 등록 -> 개발용
+    @PostMapping("/lecture/{lectureId}/grade/ratio-dev")
+    public ResponseDto createGradeRatio(@PathVariable Long lectureId, @Valid @RequestBody GradeRatioCreateRequestDto requestDto) {
+        return gradeRatioService.createGradeRatio(lectureId, requestDto);
+    }
+
+    //비율 수정
+    @PostMapping("/lecture/{lectureId}/grade/ratio")
+    public ResponseDto updateGradeRatio(@PathVariable Long lectureId, @Valid @RequestBody GradeRatioUpdateRequestDto requestDto) {
+        return gradeRatioService.updateGradeRatio(lectureId, requestDto);
+    }
+
+    //비율 조회
+    @GetMapping("/lecture/{lectureId}/grade/ratio")
+    public ResponseDto findGradeRatio(@PathVariable Long lectureId) {
+        return gradeRatioService.findGradeRatio(lectureId);
+    }
+}
