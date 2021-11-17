4fc26372137b40b1184ca1661eda57c3e0fbee70
MinjeongKong <kong1301@ajou.ac.kr>
2021-11-18 00:55:59 +0900

    Update GradeRatioApiControllerTest

--- src/test/java/com/connect/oneboardserver/web/grade/GradeRatioApiControllerTest.java
+++ src/test/java/com/connect/oneboardserver/web/grade/GradeRatioApiControllerTest.java
@@ -4,10 +4,12 @@ import com.connect.oneboardserver.domain.grade.GradeRatio;
 import com.connect.oneboardserver.domain.grade.GradeRatioRepository;
 import com.connect.oneboardserver.domain.lecture.Lecture;
 import com.connect.oneboardserver.domain.lecture.LectureRepository;
+import com.connect.oneboardserver.domain.relation.GradeRatioLecture;
 import com.connect.oneboardserver.domain.relation.GradeRatioLectureRepository;
 import com.connect.oneboardserver.web.dto.ResponseDto;
 import com.connect.oneboardserver.web.dto.grade.GradeRatioCreateRequestDto;
 import com.connect.oneboardserver.web.dto.grade.GradeRatioResponseDto;
+import com.connect.oneboardserver.web.dto.grade.GradeRatioUpdateRequestDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -17,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -50,7 +53,67 @@ public class GradeRatioApiControllerTest {
     }
 
     @Test
-    @DisplayName("학점 비율 입력")
+    @DisplayName("학점 비율 수정 요청")
+    void requestUpdateGradeRatio() {
+        // given
+        String lectureTitle = "test lecture";
+        String lecturePlan = "test url";
+        String semester = "2021-2";
+
+        Lecture lecture = lectureRepository.save(Lecture.builder()
+                .title(lectureTitle)
+                .lecturePlan(lecturePlan)
+                .semester(semester)
+                .build());
+
+        Long lectureId = lectureRepository.save(lecture).getId();
+        Integer aratio = 30;
+        Integer bRatio = 70;
+
+        GradeRatio gradeRatio = gradeRatioRepository.save(GradeRatio.builder()
+                .aratio(aratio)
+                .bratio(bRatio)
+                .build());
+
+        GradeRatioLecture gradeRatioLecture = gradeRatioLectureRepository.save(GradeRatioLecture.builder()
+                .lecture(lecture)
+                .gradeRatio(gradeRatio)
+                .build());
+
+        Integer updateA = 20;
+        Integer updateB = 80;
+
+        GradeRatioUpdateRequestDto requestDto = GradeRatioUpdateRequestDto.builder()
+                .aratio(updateA)
+                .bratio(updateB)
+                .build();
+
+
+        String url = "http://localhost:" + port + "/lecture/{lectureId}/grade/ratio";
+
+        // when
+        ResponseEntity<ResponseDto> responseEntity
+                = restTemplate.postForEntity(url, requestDto, ResponseDto.class, lectureId);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        // responseData: LinkedHashMap
+        Object responseData = responseEntity.getBody().getData();
+
+        ObjectMapper mapper = new ObjectMapper();
+        GradeRatioResponseDto responseDto = mapper.convertValue(responseData, GradeRatioResponseDto.class);
+
+        GradeRatio newGradeRatio = gradeRatioRepository.findById(responseDto.getGradeRatioId()).orElseThrow();
+
+        assertThat(newGradeRatio.getAratio()).isEqualTo(updateA);
+        assertThat(newGradeRatio.getBratio()).isEqualTo(updateB);
+
+    }
+
+
+//    @Test
+//    @DisplayName("학점 비율 입력")
     void requestCreateGradeRatio() {
         // given
         String lectureTitle = "test lecture";
