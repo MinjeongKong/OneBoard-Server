900ef4083a37c6f98d8389c454433716090f663c
MinjeongKong <kong1301@ajou.ac.kr>
2021-11-25 01:27:25 +0900

    Add assignment fileUrl service

--- src/main/java/com/connect/oneboardserver/service/assignment/AssignmentService.java
+++ src/main/java/com/connect/oneboardserver/service/assignment/AssignmentService.java
@@ -4,10 +4,13 @@ import com.connect.oneboardserver.domain.assignment.Assignment;
 import com.connect.oneboardserver.domain.assignment.AssignmentRepository;
 import com.connect.oneboardserver.domain.lecture.Lecture;
 import com.connect.oneboardserver.domain.lecture.LectureRepository;
+import com.connect.oneboardserver.service.storage.StorageService;
 import com.connect.oneboardserver.web.dto.ResponseDto;
 import com.connect.oneboardserver.web.dto.assignment.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
@@ -19,6 +22,8 @@ import java.util.List;
 @Service
 public class AssignmentService {
 
+    @Qualifier("FileStorageService")
+    private final StorageService storageService;
     private final AssignmentRepository assignmentRepository;
     private final LectureRepository lectureRepository;
 
@@ -33,6 +38,28 @@ public class AssignmentService {
 
         assignmentRepository.save(assignment);
         AssignmentResponseDto responseDto = new AssignmentResponseDto(assignment);
+        return new ResponseDto("SUCCESS", responseDto);
+    }
+
+    @Transactional
+    public ResponseDto createAssignmentFile(Long lectureId, AssignmentCreateRequestDto requestDto, MultipartFile file) throws Exception{
+        Lecture lecture = lectureRepository.findById(lectureId)
+                .orElseThrow(()->new IllegalArgumentException("해당 과목이 없습니다. id="+lectureId));
+
+        Assignment assignment = requestDto.toEntity();
+        assignment.setLecture(lecture);
+
+        assignmentRepository.save(assignment);
+        AssignmentResponseDto responseDto = new AssignmentResponseDto(assignment);
+
+        String uploadedFile = null;
+        if (file != null) {
+            String path = "/lecture_" + lectureId + "/assignment_" + assignment.getId();
+            uploadedFile = storageService.store(path, file);
+
+            assignment.setFileUrl(uploadedFile);
+        }
+
         return new ResponseDto("SUCCESS", responseDto);
     }
 
@@ -52,6 +79,34 @@ public class AssignmentService {
             return new ResponseDto("SUCCESS", responseDto);
         }
     }
+
+    @Transactional
+    public ResponseDto updateAssignmentFile(Long lectureId, Long assignmentId, AssignmentUpdateRequestDto requestDto, MultipartFile file)throws Exception {
+        Assignment assignment = assignmentRepository.findById(assignmentId)
+                .orElseThrow(()->new IllegalArgumentException("해당 과제가 없습니다. id="+assignmentId));
+
+        if (!assignment.getLecture().getId().equals(lectureId)) {
+            return new ResponseDto("FAIL");
+        } else {
+            if (assignment.getFileUrl() != null) {
+                if (storageService.delete(assignment.getFileUrl())) {
+                    assignment.setFileUrl(null);
+                }
+            }
+            assignment.update(requestDto.getTitle(),requestDto.getContent(),requestDto.getFileUrl(),
+                    requestDto.getStartDt(),requestDto.getEndDt(), requestDto.getExposeDt(), requestDto.getScore());
+
+            if (file != null) {
+                String path = "/lecture_" + lectureId + "/assignment_" + assignment.getId();
+                String uploadedFile = storageService.store(path, file);
+
+                assignment.setFileUrl(uploadedFile);
+            }
+            AssignmentResponseDto responseDto = new AssignmentResponseDto(assignment);
+            return new ResponseDto("SUCCESS", responseDto);
+        }
+    }
+
 
     @Transactional
     public ResponseDto deleteAssignment(Long lectureId, Long assignmentId) {
