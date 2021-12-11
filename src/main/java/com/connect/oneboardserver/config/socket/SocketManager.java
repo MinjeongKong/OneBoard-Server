df0dfd24b9fb3e70d50afbccd17326cd957c31f0
shy625 <thgusdud123@gmail.com>
2021-12-11 22:11:05 +0900

    Add "init connection" socket eventListener

--- src/main/java/com/connect/oneboardserver/config/socket/SocketManager.java
+++ src/main/java/com/connect/oneboardserver/config/socket/SocketManager.java
@@ -6,15 +6,22 @@ import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.listener.ConnectListener;
 import com.corundumstudio.socketio.listener.DataListener;
+import com.corundumstudio.socketio.namespace.Namespace;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 @Component
 public class SocketManager implements ApplicationListener<ContextClosedEvent> {
 
     private Configuration config;
     private SocketIOServer server;
+
+    private ConcurrentMap<String, UUID> roomHosts = new ConcurrentHashMap<>();
 
     public SocketManager() {
         config = new Configuration();
@@ -34,13 +41,31 @@ public class SocketManager implements ApplicationListener<ContextClosedEvent> {
             }
         });
 
-        server.addEventListener("init", ChatObject.class, new DataListener<>() {
+        server.addEventListener("init connection", InitObject.class, new DataListener<>() {
             @Override
-            public void onData(SocketIOClient client, ChatObject data, AckRequest ackRequest) {
-                System.out.println("userName : " + data.getUserName());
-                System.out.println("message : " + data.getMessage());
+            public void onData(SocketIOClient client, InitObject data, AckRequest ackRequest) {
+                System.out.println("userType = " + data.getUserType());
+                System.out.println("session = " + data.getRoom());
 
-                client.sendEvent("echo", data.getMessage());
+                Namespace mainNamespace = (Namespace) server.getNamespace("");
+
+                mainNamespace.joinRoom(data.getRoom(), client.getSessionId());
+
+//                System.out.println("Room :");
+//                for (String room : mainNamespace.getRooms()) {
+//                    System.out.println("\t" + room);
+//                }
+//                System.out.println(data.getRoom() + " Room Clients :");
+//                for (SocketIOClient roomClient : mainNamespace.getRoomClients(data.getRoom())) {
+//                    System.out.println("\t" + roomClient.getSessionId());
+//                }
+
+                if (data.getUserType().equals("T")) {
+                    roomHosts.putIfAbsent(data.getRoom(), client.getSessionId());
+//                    System.out.println("roomHosts : " + roomHosts);
+                }
+
+                client.sendEvent("echo", "userType : " + data.getUserType() + " room : " + data.getRoom());
             }
         });
 
